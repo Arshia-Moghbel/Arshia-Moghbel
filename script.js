d9266a23c778024050504b222d527b9829d3d60b
Arshia Moghbel <arshiamoghbel@icloud.com>
2025-05-20 12:08:57 +0330

    update site commit

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleLang() {
-  const lang = document.documentElement.lang;
-  if (lang === "fa") {
-    window.location.href = "index-en.html";
-  } else {
-    window.location.href = "index.html";
-  }
-}
